a3a09cedc34fc616b043a0b5c519ddff1181a131
Roberto Morgado Luengo <robertomorgadoluengo@iMac-de-Roberto.local>
2022-09-28 20:31:04 +0200

    Scafolding inicial

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
+import { Typography } from '@mui/material'
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { ShopLayout } from '../components/layouts'
 
 const Home: NextPage = () => {
   return (
-    <h1> Next app</h1>
+    <ShopLayout title={'Teslo-shop - Home'} pageDescription={ 'Encuentra los mejores productos tesla aqui'}>
+      <Typography variant='h1' component='h1'>Tienda</Typography>
+      <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
+    </ShopLayout>
   )
 }
 
