84808272ef00b6359c19021a0965b4d843342771
Roberto Morgado Luengo <robertomorgadoluengo@iMac-de-Roberto.local>
2022-11-07 13:07:04 +0100

    Homepage temporal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,31 @@
-import { Typography } from '@mui/material'
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material'
 import type { NextPage } from 'next'
 import { ShopLayout } from '../components/layouts'
+import { initialData } from '../database/products'
+
 
 const Home: NextPage = () => {
   return (
     <ShopLayout title={'Teslo-shop - Home'} pageDescription={ 'Encuentra los mejores productos tesla aqui'}>
       <Typography variant='h1' component='h1'>Tienda</Typography>
       <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
+
+      <Grid container spacing={4}>
+        {
+         initialData.products.map( product => (
+          <Grid item xs={6} sm={4} key={ product.slug }>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component='img'
+                  image={`products/${ product.images[0] }`}
+                  alt={ product.title } />                
+              </CardActionArea>
+            </Card>
+          </Grid>
+         )) 
+        }        
+      </Grid>
     </ShopLayout>
   )
 }
