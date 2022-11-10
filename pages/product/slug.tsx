b843618c71e17044bf3746ffab029ad8ccf073f5
Roberto Morgado Luengo <robertomorgadoluengo@iMac-de-Roberto.local>
2022-11-10 17:23:59 +0100

    Slideshow de las imagenes 185

--- pages/product/slug.tsx
+++ pages/product/slug.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, Chip, Grid, Typography } from '@mui/material';
 import React from 'react'
 import { ShopLayout } from '../../components/layouts';
+import { ProductSlideshow } from '../../components/products';
 import { initialData } from '../../database/products';
 
 const product = initialData.products[0];
@@ -11,7 +12,8 @@ export const ProductPage = () => {
    <ShopLayout title={ product.title } pageDescription={ product.description }>
       <Grid container spacing={3}>
         <Grid item xs={12} sm={7}>
-        {/** Slideshow */}  
+        {/** Slideshow */}
+        <ProductSlideshow images={ product.images }/>
         </Grid>
         <Grid item xs={12} sm={5}>
           <Box display='flex' flexDirection='column'>
