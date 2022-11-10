cb14f8c51341adbe3f3a999f24d6650e8ca38ece
Roberto Morgado Luengo <robertomorgadoluengo@iMac-de-Roberto.local>
2022-11-10 10:09:20 +0100

    Componente para mostrar productos 182

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material'
 import type { NextPage } from 'next'
 import { ShopLayout } from '../components/layouts'
+import { ProductList } from '../components/products'
 import { initialData } from '../database/products'
+import { IProduct } from '../interfaces'
 
 
 const Home: NextPage = () => {
@@ -10,22 +12,9 @@ const Home: NextPage = () => {
       <Typography variant='h1' component='h1'>Tienda</Typography>
       <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
 
-      <Grid container spacing={4}>
-        {
-         initialData.products.map( product => (
-          <Grid item xs={6} sm={4} key={ product.slug }>
-            <Card>
-              <CardActionArea>
-                <CardMedia
-                  component='img'
-                  image={`products/${ product.images[0] }`}
-                  alt={ product.title } />                
-              </CardActionArea>
-            </Card>
-          </Grid>
-         )) 
-        }        
-      </Grid>
+      <ProductList 
+        products={ initialData.products as any }
+        />
     </ShopLayout>
   )
 }
