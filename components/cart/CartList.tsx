aa950d964e48fa0da06d0791ff29741a03370767
Roberto Morgado Luengo <robertomorgadoluengo@iMac-de-Roberto.local>
2022-11-13 21:47:57 +0100

    Mostrar prorductos en el carrito de compras 190

--- components/cart/CartList.tsx
+++ components/cart/CartList.tsx
@@ -1,5 +1,7 @@
-import { Typography } from "@mui/material"
+import { Box, Button, CardActionArea, CardMedia, Grid, Link, Typography } from "@mui/material"
 import { initialData } from "../../database/products"
+import NextLink from 'next/link'
+import { ItemCounter } from "../ui"
 
 const productsInCart = [
     initialData.products[0],
@@ -12,7 +14,33 @@ export const CartList = () => {
     <>
         {
             productsInCart.map( product => (
-                <Typography key={product.slug}>{ product.title }</Typography>
+                <Grid container spacing='2' key={product.slug} sx={{ mb: 1 }}>
+                    <Grid item xs={3}>
+                        <NextLink href='/product/slug'>
+                            <Link>
+                                <CardActionArea>
+                                    <CardMedia
+                                        image={ `products/${ product.images[0] }`}
+                                        component='img'
+                                        sx={{ borderRadius: '5px' }}
+                                    />
+                                </CardActionArea>
+                            </Link>
+                        </NextLink>
+                    </Grid>
+                    <Grid item xs={7}>
+                        <Box display='flex' flexDirection='column'>
+                            <Typography variant='body1'>{ product.title }</Typography>
+                            <Typography variant='body1'>Talla: <strong>M</strong></Typography>
+                            <ItemCounter/>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={2} display='flex' alignItems='center' flexDirection='column'>
+                        <Typography variant="subititle1">{ `$${ product.price }` }</Typography>
+                        <Button variant="text" color="secondary">Remover</Button>
+                    </Grid>
+                </Grid>
+
             ))
         }
     </>
