5131ee0053b0ca16e8bf82512b09d62e9633a502
Roberto Morgado Luengo <robertomorgadoluengo@iMac-de-Roberto.local>
2022-11-14 22:17:13 +0100

    Mongoose y Modelo de Producto 204

---
+++ models/Product.ts
@@ -0,0 +1,70 @@
+import mongoose, { Schema, model, Model } from 'mongoose'
+
+const productSchema = new Schema ({
+    description: {
+        type: String,
+        required: true
+    },
+    images: [
+        {
+            type: String
+        }
+    ],
+    inStock: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    price: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    sizes: [
+        {
+            type: String,
+            enum: {
+                values: [ 'XS', 'S', 'M', 'L', 'XL', 'XL', 'XXL', 'XXXL' ],
+                message: '{VALUE} no es un tamaño permitido'                
+            }           
+        }
+    ],
+    slug: {
+        type: String,
+        required: true,
+        unique: true
+    },    
+    tags: [
+        {
+            type: String
+        }
+    ],
+    title: {
+        type: String,
+        required: true
+    },
+    type:  {
+        type: String,
+        enum: {
+            values: [ 'shirts', 'pants', 'hoodies', 'hats'],
+            message: '{VALUE} no es un tipo válido'                
+        }           
+    },
+    gender:  {
+        type: String,
+        enum: {
+            values: [ 'men', 'women', 'kid', 'unisex'],
+            message: '{VALUE} no es un genero válido'                
+        }           
+    }
+}, {
+    timestamps: true
+});
+
+
+// TODO: Crear indice mongo
+
+
+const Product: Model<IProduct> = mongoose.models.Product || model('Product', productSchema);
+
+export default Product;
