d22adc26408e89bfb28ff329234cb1d2f04d7089
Roberto Morgado Luengo <robertomorgadoluengo@iMac-de-Roberto.local>
2022-11-15 13:10:02 +0100

    Mostrar el menu activo 220

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,8 +1,13 @@
 import NextLink from 'next/link';
 import { AppBar, Toolbar, Typography, Link, Box, Button, IconButton, Badge } from "@mui/material"
 import { SearchOutlined, ShoppingCartOutlined } from '@mui/icons-material';
+import { useRouter } from 'next/router';
 
 export const Navbar = () => {
+
+    // para saber en que ruta estoy
+    const router = useRouter();
+
   return (
     <AppBar>
         <Toolbar>
@@ -20,17 +25,17 @@ export const Navbar = () => {
             <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
                 <NextLink href='/category/men' passHref>
                     <Link>
-                        <Button>Hombres</Button>
+                        <Button color={ router.asPath === '/category/men' ? 'primary' : 'info'}>Hombres</Button>
                     </Link>
                 </NextLink>
                 <NextLink href='/category/women' passHref>
                     <Link>
-                        <Button>Mujeres</Button>
+                        <Button color={ router.asPath === '/category/women' ? 'primary' : 'info'}>Mujeres</Button>
                     </Link>
                 </NextLink>
                 <NextLink href='/category/kid' passHref>
                     <Link>
-                        <Button>Niños</Button>
+                        <Button color={ router.asPath === '/category/kid' ? 'primary' : 'info'}>Niños</Button>
                     </Link>
                 </NextLink>
             </Box>
